9469f7919c33a00c85a6920b396762db272a84a6
Loser Teach <jetaimefrc@gmail.com>
2018-09-04 16:12:26 +0700

    hihi

---
+++ forFun.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const fs = require('fs');
+
+fs.readFile('Mnemonic.txt', (err, data) => {
+    if (err) throw err;
+    let listKanji = JSON.parse(data);
+    console.log(listKanji);
+});
+
+console.log('This is after the read call'); 
+/// Cann't running ! ^_^ 
